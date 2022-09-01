b4eb415c161a9ed41be58ad5dc887d8a8c7d5c08
aditya22081998 <123170048@student.upnyk.ac.id>
2022-09-01 17:48:21 +0700

    Tambahan

--- TesOpenCV/oldmain.cpp
+++ TesOpenCV/oldmain.cpp
@@ -33,45 +33,39 @@ void setLabel(cv::Mat& im, const std::string label, std::vector<cv::Point>& cont
 
     cv::Size text = cv::getTextSize(label, fontface, scale, thickness, &baseline);
     cv::Rect r = cv::boundingRect(contour);
-
     cv::Point pt(r.x + ((r.width - text.width) / 2), r.y + ((r.height + text.height) / 2));
+    
+    cv::Point centerPoint(r.x + r.width/2, r.y+r.height/2);
+	
+				
+    // cv::Point A(r.x,r.y);
+	// cv::Point B(r.x+r.width,r.y);
+	// cv::Point C(r.x+r.width,r.y+r.height);
+	// cv::Point D(r.x,r.y+r.height);
+				
+	// circle(im, A, 5, Scalar(255), 2, 8, 0);
+	// circle(im, B, 5, Scalar(255), 2, 8, 0);
+	// circle(im, C, 5, Scalar(255), 2, 8, 0);
+	// circle(im, D, 5, Scalar(255), 2, 8, 0);
+    
+    // circle(im, centerPoint, 5, Scalar(255), 2, 8, 0);
+    
     cv::rectangle(im, pt + cv::Point(0, baseline), pt + cv::Point(text.width, -text.height), CV_RGB(255,255,255), CV_FILLED);
     cv::putText(im, label, pt, fontface, scale, CV_RGB(0,0,0), thickness, 8);
 }
 
-//~ void findCornerHarris(Mat image_clone){
-//~ int blockSize = 2;
-//~ int apertureSize = 3;	
-//~ double k = 0.04;
-	
-	//~ Mat dst= Mat::zeros (image_clone.size(), CV_32FC1); 
-				//~ cornerHarris(image_clone, dst, blockSize, apertureSize, k);
-
-				//~ Mat dst_norm, dst_norm_scaled;
-
-				//~ cv::normalize( dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, Mat() );
-				//~ cv::convertScaleAbs( dst_norm, dst_norm_scaled );
-
-				//~ for (int i=0; i<dst_norm.rows; i++){
-					//~ for( int j = 0; j < dst_norm.cols; j++ ){
-						//~ if( (int) dst_norm.at<float>(i,j) > 100 ){
-							//~ cv::circle( dst_norm_scaled, cv::Point(j,i), 5,  cv::Scalar(255), 2, 8, 0 );
-						//~ }
-					//~ }
-				//~ }
-	//~ imshow ("Corner harris", dst_norm_scaled);
-//~ }
-
 int main() {
     
-Mat image,gray,blurImage,edgeCanny, drawing, zeros, dilate;
+Mat image,gray,blurImage,edgeCanny, drawing, zeros, dilate,dilate2, erode;
 cv::Size kernel(5,5);
 
 
 VideoCapture cap(0);
-
+cap.set (CV_CAP_PROP_FPS, 10);
 double fps = cap.get(CV_CAP_PROP_FPS);
 cout << "Frames per second using video.get(CAP_PROP_FPS) : " << fps << endl;
+cap.set(CAP_PROP_FRAME_WIDTH, 320);
+cap.set(CAP_PROP_FRAME_HEIGHT, 240);
 
 if (!cap.isOpened()){
 
@@ -85,27 +79,32 @@ cap >> image;
 
 cvtColor(image, gray, CV_BGR2GRAY);
 
-GaussianBlur(gray, blurImage, kernel, 0);
+cv::dilate(gray, dilate, Mat(), Point(-1,-1), 1, 1, 0) ;
 
-Canny(blurImage,edgeCanny, 40,200);
+GaussianBlur(dilate, blurImage, kernel, 0);
 
-cv::dilate(edgeCanny, dilate, Mat(), Point(-1,-1), 1, 1, 0);
+cv::erode(blurImage, erode, Mat(), Point(-1,-1), 1, 1, 0 );
 
-Mat image_copy= dilate.clone();
+Canny(erode,edgeCanny, 40,200); 
+
+Mat image_copy= edgeCanny.clone();
+
 std::vector<std::vector<cv::Point> > contours;
 
 findContours(image_copy, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
 std::vector<cv::Point> approx;
 
-Mat pointContour= image.clone();
+//~ Mat pointContour= image.clone();
 
-//~ Mat drawing= Mat zeros (image_copy.size(), CV_8UC3);
-
+Mat drawing= Mat::zeros (image_copy.size(), CV_8UC3);
+Mat drawingcenter = Mat::zeros (drawing.size(), CV_8UC3);
 for (unsigned int i= 0; i<contours.size(); i++){
 		// Approximate contour with accuracy proportional
 		// to the contour perimeter
-		cv::approxPolyDP(cv::Mat(contours[i]), approx, cv::arcLength(cv::Mat(contours[i]), true)*0.02, true);
+		double peri = cv::arcLength(cv::Mat(contours[i]), true);
+		
+		cv::approxPolyDP(cv::Mat(contours[i]), approx, peri*0.02, true);
 		
 		// Skip small or non-convex objects 
 		if (std::fabs(cv::contourArea(contours[i])) < 100 || !cv::isContourConvex(approx))
@@ -131,13 +130,15 @@ for (unsigned int i= 0; i<contours.size(); i++){
 				// Detect rectangle or square
 			    cv::Rect r = cv::boundingRect(contours[i]);
 				double ratio = std::abs(1 - (double)r.width / r.height);
-				setLabel(pointContour, ratio <= 0.02 ? "SQUARE" : "RECTANGLE", contours[i]);
+				
+				setLabel(drawing, ratio <= 0.02 ? "SQUARE" : "RECTANGLE", contours[i]);
+				drawContours( drawing, contours, (int)i, Scalar(255), 2, LINE_8, approx, 0 );
 			 }
 		}	    
 	}
-//findCornerHarris(image_copy);
+imshow("Drawing Rectangle",drawing);
 imshow("Edge Detection", image_copy);
-imshow("Rectangle", pointContour);
+// imshow("Drawing Center", drawingcenter);
 if (waitKey(25)== (0x20))
 	break;
 }
